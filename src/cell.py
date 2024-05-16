b54edcf5b18333bec442b6dcbf95cf168992df40
djd223 <davissd99@LAPTOP-H9C5DSR3>
2024-05-16 09:43:36 -0700

    draw between center of cells with draw_move

--- src/cell.py
+++ src/cell.py
@@ -29,3 +29,16 @@ class Cell:
     if self.has_bottom_wall:
       line = Line(Point(x1, y2), Point(x2, y2))
       self._win.draw_line(line)
+    
+  def draw_move(self, to_cell, undo=False):
+    fill_color = "red"
+    if undo:
+      fill_color = "gray"
+    
+    x1 = (self._x1 + self._x2) / 2
+    y1 = (self._y1 + self._y2) / 2
+    x2 = (to_cell._x1 + to_cell._x2) / 2
+    y2 = (to_cell._y1 + to_cell._y2) / 2
+
+    l = Line(Point(x1,y1), Point(x2,y2))
+    self._win.draw_line(l, fill_color)
